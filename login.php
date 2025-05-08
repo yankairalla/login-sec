18c4f86a78f0c580455cce7c6de6d7e8803983b7
Yan Kairalla <yanmkairalla@hotmail.com>
2025-05-08 00:31:02 -0300

    Commit inicial

---
+++ login.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+
+
+?>
+
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+</head>
+<body>
+    <form>
+        <label for="user">Usuario: </label>
+        <input name="user" required/>
+        <label for="pass">Senha: </label>
+        <input name="pass" type="password" required/>
+    </form>
+</body>
+</html>
+
+
+
